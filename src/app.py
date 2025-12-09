8aae7b21b04c8ed88f5a0b093b35c9cbae7df271
Punit Turlapati <Punitsst@gmail.com>
2025-12-09 10:20:50 -0600

    Try ci/cd

--- src/app.py
+++ src/app.py
@@ -1,2 +1,40 @@
-def add (a, b):
+import math
+
+def add(a:int | float, b: int | float) -> int | float:
     return a+b
+
+def subtract(a: int | float, b: int | float) -> int | float:
+    return a-b
+
+def multiply(a: int | float, b: int | float) -> int | float:
+    return a*b
+
+def divide(a: int | float, b: int | float) -> int | float:
+    if b == 0:
+        raise ValueError("Division by zero is not allowed.") 
+    
+    return a / b
+
+
+def square(a: int | float) -> int | float:
+    return a * a
+
+def sin(x: int | float) -> int | float:
+    return math.sin(x)
+
+
+def cos(x: int | float) -> int | float:
+    return math.cos(x)
+
+def tan(x: int | float) -> int | float:
+    return math.tan(x)
+
+
+def square_root(x: int | float) -> int | float:
+    if x < 0:
+        raise ValueError("Cannot compute square root of negative number.")
+    
+    return math.sqrt(x)
+
+
+def percentage
